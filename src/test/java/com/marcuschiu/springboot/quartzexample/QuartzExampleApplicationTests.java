5879e5ab1f9857cb7fb3c55d2cbbdc5be8dff861
Marcus Chiu <marcuschiu9@gmail.com>
2021-04-01 21:30:23 -0500

    initial commit

---
+++ src/test/java/com/marcuschiu/springboot/quartzexample/QuartzExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcuschiu.springboot.quartzexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuartzExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
